abe91a329c23013d1872057704215158b4cf1dd9
tarasinski.dawid <tarasinski.dawid@STACJA105-10>
2023-09-27 14:29:22 +0200

    Dodaj pliki projektów.

---
+++ WpfApp2/MyDog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class MyDog : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
+}
